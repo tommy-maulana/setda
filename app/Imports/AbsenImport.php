62672eb996dc9c143aa72dab21cf8478506ad420
Abdul Aziz Zulfikar <aaz.abdulazizzulfikar@gmail.com>
2021-01-06 13:57:23 +0700

    update casting using strtotime

--- app/Imports/AbsenImport.php
+++ app/Imports/AbsenImport.php
@@ -14,11 +14,11 @@ class AbsenImport implements ToModel
     */
     public function model(array $row)
     {
-        $a = strptime($row[1], '%Y-%m-%d %H:%i');
-        $timestamp = mktime(0, 0, 0, $a['tm_mon']+1, $a['tm_mday'], $a['tm_year']+1900);
+        $str = str_replace('/','-',$row[1]);
+        
         return new DataRawAbsensi([
             'id_kry'        => $row[0],
-            'tgl'           => $timestamp,
+            'tgl'           => strtotime($str);,
             'kd_absen'      => $row[2],
             'id_mesin'      => $row[3]
         ]);
